a946340ac60d323f1776f1521530d19530c3c1b4
cem <cemkagank@gmail.com>
2020-10-19 20:35:15 +0300

    update

--- off-lab/main.py
+++ off-lab/main.py
@@ -10,10 +10,18 @@ print(pharase)
 print(pharase.index("K"))
 print(pharase.index("KW"))
 print(pharase.replace("KW","FF"))
-darray = ["Male","Johhny Test","Johhny Bravo",5,False ]
+darray = ["Male","Johhny Test","Johhny Bravo",5,False,5 ]
 print(darray[3:])
 print(darray[-1])
 print(darray[2:4])
+print(darray.count(5))
+# darray.append(thing) *adds thing to list
+# darray.extend(listname) *merges the lists
+# darray.insert(number, thing) *ads the thing to the specified position
+# darray.remove()
+# darray.pop() *removes the last element
+# darray.sort() *sorts the list in alphabetical order or ascending order
+# darray.reverse() *uno
 
 
 
