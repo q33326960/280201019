5c070ade3887de5dbc6bb6884d7e4807b0c3a006
cem <cemkagank@gmail.com>
2020-10-19 15:43:01 +0300

    example 6 Remastered

--- lab2/example6REAMSTERED.py
+++ lab2/example6REAMSTERED.py
@@ -1,1 +1,6 @@
-
+m = eval(input("Mesafe ? (km) "))
+v1 = eval(input("Velocity 1 (km/h) ? "))
+v2 = eval(input("Velocity 2 (km/h) ? "))
+y = eval(input("Arada kalan mesafe (km) ? "))
+t = (m-y) / (v1 + v2) *60
+print("Geçen süre", t , "dakika")
