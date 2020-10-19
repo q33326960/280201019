3482660a45298a28fee0baa060465b8e4fa5b4f2
cem <cemkagank@gmail.com>
2020-10-19 17:04:33 +0300

    offlab update and comment on ex6re

--- lab2/example6REMASTERED.py
+++ lab2/example6REMASTERED.py
@@ -4,3 +4,4 @@ v2 = eval(input("Velocity 2 (km/h) ? "))
 y = eval(input("Arada kalan mesafe (km) ? "))
 t = (m-y) / (v1 + v2) *60
 print("Geçen süre", t , "dakika")
+# Enter 490, 70, 80, 150 and you should get 136.0 #
---
+++ off-lab/main.py
@@ -0,0 +1,15 @@
+print("pontifex\nmaximus")
+print("\"KEKW\"")
+pharase = "KEKW!"
+print(pharase.lower() + " girls are dumb and they are clearly inferior to men")
+print(pharase.isupper())
+print(pharase.lower().islower())
+pharase.lower()
+print(pharase.isupper())
+print(pharase)
+print(pharase.index("K"))
+print(pharase.index("KW"))
+print(pharase.replace("KW","FF"))
+
+
+
