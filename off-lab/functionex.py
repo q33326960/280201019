523f70f4ff68123430a99f50b906bf1c5d6d4660
cem <cemkagank@gmail.com>
2020-10-20 20:58:02 +0300

    rice field update

---
+++ off-lab/functionex.py
@@ -0,0 +1,21 @@
+def greet(dil):
+    if dil == "tr":
+        print("Selam")
+    elif dil == "en":
+        print("Hi")
+    elif dil == "ch":
+        print("Welcome to the Rice Fields motherfucker")
+    elif dil == "jp":
+        print("Yamete onii-chan itaiiii")
+    else:
+        print("I don't know what garbage language you are talking")
+
+greet("ch")
+greet("tr")
+greet("jp")
+greet("es")
+
+# def greet(dil):
+#    if dil == "tr"
+#        return "Hello"
+# print(greet(tr))
