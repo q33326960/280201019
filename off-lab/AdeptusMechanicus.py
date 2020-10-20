4f42dd58608a8b398193128403c91bc45f45789f
cem <cemkagank@gmail.com>
2020-10-20 13:20:56 +0300

    Mechanicus

---
+++ off-lab/AdeptusMechanicus.py
@@ -0,0 +1,9 @@
+from colorama import Fore, Back, Style
+
+print(Fore.MAGENTA + "From the moment I understood the weakness of my flesh," + Fore.RESET + Fore.RED + Back.LIGHTWHITE_EX + " It disgusted me." + Back.RESET + Fore.RESET)
+print(Fore.GREEN + "I craved the strength and certainty of steel." + Fore.RESET)
+print(Fore.LIGHTCYAN_EX + "I aspired to the purity of the Blessed Machine." + Fore.RESET)
+print(Fore.LIGHTRED_EX + "Your kind cling to your flesh, as if it will not decay and fail you.")
+print("One day the crude biomass that you call a temple will wither," + Fore.RESET + Fore.RED + " and you will beg my kind to save you" + Fore.RESET )
+print(Fore.RED + Style.DIM + "But I'm already saved. for the Machine is immortal." + Fore.RESET + Style.RESET_ALL)
+print(Fore.LIGHTYELLOW_EX + "Even in death I serve the Omnissiah.")
