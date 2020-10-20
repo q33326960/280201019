4f58190639417de8bdd37ac2390680b3419024b4
cem <cemkagank@gmail.com>
2020-10-20 23:00:13 +0300

    loops and iterations

--- off-lab/Loops an Iterations.py
+++ off-lab/Loops an Iterations.py
@@ -1,21 +1,5 @@
-def isPrime(number):
-    if number < 2:
-        print("Please enter a valid integer")
-        repeat()
-    bolen = number - 1
-    while bolen >= 1:
-        kalan = number % bolen
-        bolen = bolen - 1
-        if bolen > 1:
-            if kalan == 0:
-                print("It's not a prime number")
-                repeat()
-                break
-        if bolen == 1:
-            print("It's a prime number!")
-            repeat()
-
-def repeat():
-    x = int(input("Enter the number to check if its prime or not "))
-    isPrime(x)
-repeat()
+for i in [5,4,3,2,1]:
+    print(i)
+ihate = ["leage of legends","TikTok","Instagram","Football","Fortnite" ]
+for thing in ihate:
+    print("I hate", thing)
