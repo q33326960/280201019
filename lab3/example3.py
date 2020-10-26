bca06d7f8794f6ec21a596d3abc5bb9b3c5af213
Cem <cemkagank@gmail.com>
2020-10-26 11:47:28 +0300

    ex3

---
+++ lab3/example3.py
@@ -0,0 +1,11 @@
+gpa = float(input("gpa"))
+lec = int(input("lec"))
+
+if gpa >= 2.0 :
+    if lec >= 47:
+        print("congrats")
+else:
+    print("not enough lectures")
+
+if gpa < 2:
+    print("not enogh gpa")
