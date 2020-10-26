8c6c77d56265528a70aea54205b9cd324b20101d
Cem <cemkagank@gmail.com>
2020-10-26 08:09:02 +0300

    ex

---
+++ off-lab/lec2ex.py
@@ -0,0 +1,5 @@
+cover = eval(input("Cover price: "))
+price = cover * 6 / 10
+desiredAmount = int(input("Desired amount of books: "))
+sumPrice = (price * desiredAmount) + (desiredAmount - 1) * 0.75 + 3
+print(sumPrice)
