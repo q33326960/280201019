d50df668098a7c0c8653eb1c1565e9805c8958e9
Cem <cemkagank@gmail.com>
2020-10-26 11:42:02 +0300

    ex2

--- lab3/example2.py
+++ lab3/example2.py
@@ -5,10 +5,10 @@ num3 = int(input("num3"))
 if num1 > num2:
     if num1 > num3:
         print(num1)
-if num2 > num3:
+elif num2 > num3:
     if num2 > num1:
         print(num2)
-if num3 > num2:
+elif num3 > num2:
     if num3 > num1:
         print(num3)
 else:
