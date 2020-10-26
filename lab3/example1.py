09bce1663206a5ba6a943e352d392b4cef7cc165
Cem <cemkagank@gmail.com>
2020-10-26 11:31:08 +0300

    dd

---
+++ lab3/example1.py
@@ -0,0 +1,5 @@
+num = int(input(num ?))
+if num >= 0:
+    print(num)
+if num < 0:
+    print(abs(num))
--- off-lab/lec2ex.py
+++ off-lab/lec2ex.py
@@ -1,5 +1,8 @@
-cover = eval(input("Cover price: "))
-price = cover * 6 / 10
-desiredAmount = int(input("Desired amount of books: "))
-sumPrice = (price * desiredAmount) + (desiredAmount - 1) * 0.75 + 3
-print(sumPrice)
+def ex1():
+    cover = eval(input("Cover price: "))
+    price = cover * 6 / 10
+    desiredAmount = int(input("Desired amount of books: "))
+    sumPrice = (price * desiredAmount) + (desiredAmount - 1) * 0.75 + 3
+    print(sumPrice)
+
+
