c69d8c0fbb8541f0608eda4222df8b187bddc060
Cem <cemkagank@gmail.com>
2020-11-09 12:08:16 +0300

    ex2

--- lab4/example2.py
+++ lab4/example2.py
@@ -1,9 +1,11 @@
 x = int(input("Enter the year"))
-if x <= 1000:
+if x % 100 == 0:
     if x % 400 == 0 and x % 4 == 0:
         print("Leap year")
-elif x > 1000:
+    else:
+        print("not")
+elif x % 100 != 0 :
     if x % 4 ==0:
         print("Leap year")
-else:
-    print("Not a leap year")
+    else:
+        print("not")
