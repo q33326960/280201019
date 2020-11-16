0319eb3547d6f171370d4553f623dab637c0f799
Cem <cemkagankayili@gmail.com>
2020-11-16 12:22:37 +0300

    ex3

---
+++ lab5/example3.py
@@ -0,0 +1,10 @@
+x = input("num1")
+y = input("num2")
+common = list()
+for i in x:
+    for _ in y:
+        if i in y:
+            if i not in common:
+                common.append(i)
+print(common)
+            
