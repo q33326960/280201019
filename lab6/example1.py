c943dac5adb770a94e6234a6f4c4663aaeee8df5
Cem <cemkagankayili@gmail.com>
2020-11-23 12:14:51 +0300

    yıkık

---
+++ lab6/example1.py
@@ -0,0 +1,5 @@
+x = input("email: ")
+x = x.lower()
+x = x.replace(".","")
+if x == "ceng113@hotmailcom":
+    print("same")
---
+++ off-lab/assigment.py
@@ -0,0 +1,10 @@
+x = input("Please enter your name: ")
+x = x.upper()
+for i in x:
+    print(i)
+y= ""
+c =""
+while y != "quit":
+    y = input("plesae enter str or quit to quit")
+    c = c + y 
+print(c)
