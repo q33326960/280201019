0d5bfe972029198f7d8676fcd626e3e72952ffc3
Cem <cemkagank@gmail.com>
2020-11-30 11:40:44 +0300

    ex1 lab7

--- lab7/example1.py
+++ lab7/example1.py
@@ -1,4 +1,4 @@
-inp1 = [("Sıla",21), ("Elif",20), ("Buse" ,17), ("Çıtır", 16)]
+inp1 = [("Sıla",21), ("Elif",20), ("Buse" ,17), ("Çıtır", 16), ("Alev",23)]
 inp1d = dict(inp1)
 for i in inp1d:
     if inp1d.get(i) > 18:
