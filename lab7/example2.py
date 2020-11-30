12e69684cddbe99ae93d2cf496bc3ac9c447c858
Cem <cemkagank@gmail.com>
2020-11-30 11:55:06 +0300

    ex2

---
+++ lab7/example2.py
@@ -0,0 +1,13 @@
+books = ["DAS KAPITAL","COMMUNIST MANIFESTO","KADINSIZ ERKEKLER"]
+book_dict = dict()
+charl = list()
+for i in books:
+    charl = []
+    for char in i:
+        if char in charl:
+            continue
+        else:
+            charl.append(char)
+          
+    book_dict[i] = (len(i),len(charl))
+print(book_dict)
