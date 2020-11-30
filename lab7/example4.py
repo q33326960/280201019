9695b994145e9a8f1aee5d9302e8ed899645939a
Cem <cemkagank@gmail.com>
2020-11-30 12:34:22 +0300

    ex4

--- lab7/example4.py
+++ lab7/example4.py
@@ -3,6 +3,6 @@ for i in range(5):
     proleter = input("Proleter: ")
     maas = int(input("Maas:"))
     emp_dict[proleter] = maas
-sortedl=sorted(emp_dict.items(), key=lambda x:x[1], reverse=True)
+sortedl=sorted(emp_dict.items(), reverse=True)
 for i in range(3):
     print(sortedl[i])
