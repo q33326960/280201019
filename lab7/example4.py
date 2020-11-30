3bd2184daf625e4d46de5467d569cacb637c9ec9
Cem <cemkagank@gmail.com>
2020-11-30 12:35:04 +0300

    ex4

--- lab7/example4.py
+++ lab7/example4.py
@@ -1,7 +1,7 @@
 emp_dict = dict()
 for i in range(5):
     proleter = input("Proleter: ")
-    maas = int(input("Maas:"))
+    maas = int(input("Maas: "))
     emp_dict[proleter] = maas
 sortedl=sorted(emp_dict.items(), reverse=True)
 for i in range(3):
