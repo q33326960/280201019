e02ce2c11bdd8ac08db8c5e5af5b69905b3752cd
Cem <cemkagank@gmail.com>
2021-01-18 12:34:41 +0300

    d

--- lab12/example1.py
+++ lab12/example1.py
@@ -1,6 +1,6 @@
 def selectionSort(list):
     count = 0
-    for x in range(len(list)):
+    for _ in range(len(list)):
         minx = None
         for i in list[count:]:
             if  minx == None or i < minx:
@@ -13,5 +13,7 @@ def selectionSort(list):
 sa = [19,-3,4,24,12,7,9,69,0,16,35,42,63]
 selectionSort(sa)
 print(sa)
-
+my_list = [22, 8, 12, -4, 27, 30, 36, 50, 7, 68, 91, 56, 2, 85, 42, 98, 25]
+selectionSort(my_list)
+print(my_list)
 
